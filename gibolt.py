8cab1281f59f73e599d7bffdb4a3c7e69e2eecfa
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2015-04-10 17:03:48 +0200

    Clean the code

--- gibolt.py
+++ gibolt.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
-# coding: utf-8
 
-import locale
-from datetime import datetime
+from datetime import date
+
 import requests
 from dateutil.relativedelta import relativedelta
 from flask import (
     Flask, request, session, render_template, redirect, url_for)
 
 app = Flask(__name__)
-app.config.from_envvar('GIBOLT_SETTINGS')
-
-locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
+app.secret_key = 'secret'
+app.config['ORGANISATION'] = 'Kozea'
+app.config.from_envvar('GIBOLT_SETTINGS', silent=True)
 
 cache = {'users': {}, 'repos': {}}
+
+
+def date_from_iso(iso_date):
+    return date(*[int(value) for value in iso_date[:10].split('-')])
 
 
 @app.route('/')
@@ -44,26 +47,25 @@ def logout():
 
 
 def get_allowed_repos(username, password):
-    """ return a list of repo names allowed for this user
-    """
+    """Return a list of repo names allowed for this user."""
     # retrive all repositories from github
-    r = requests.get(
-        'https://api.github.com/orgs/Kozea/repos?type=all',
+    response = requests.get(
+        'https://api.github.com/orgs/{0}/repos?type=all'.format(
+            app.config['ORGANISATION']),
         auth=(username, password))
-    if r.status_code != 200:
+    if response.status_code != 200:
         return None
-    repos = r.json()
-    while r.links.get('next'):
-        r = requests.get(r.links['next']['url'], auth=(username, password))
-        repos += r.json()
-        print("query for repo sent to github")
+    repos = response.json()
+    while response.links.get('next'):
+        response = requests.get(
+            response.links['next']['url'], auth=(username, password))
+        repos += response.json()
     return [repo['name'] for repo in repos]
 
 
 @app.route('/refresh')
 def refresh_all_milestones():
-    """ refresh milstones for all repositories of the current users
-    """
+    """Refresh milstones for all repositories of the current users."""
     repo_names = cache.get('users').get(session['username'])
     for repo_name in repo_names:
         refresh_repo_milestones(repo_name)
@@ -71,59 +73,46 @@ def refresh_all_milestones():
 
 
 def refresh_repo_milestones(repo_name):
-    """ refresh all milestones for a repository """
+    """Refresh all milestones for a repository."""
     username = session.get('username')
     password = session.get('password')
     repo = {}
     url = 'https://api.github.com/repos/{0}/{1}/milestones'.format(
-        app.config.get('ORGANISATION'), repo_name)
-    r = requests.get(url, auth=(username, password))
-    repo['milestones'] = r.json()
+        app.config['ORGANISATION'], repo_name)
+    response = requests.get(url, auth=(username, password))
+    repo['milestones'] = response.json()
     for milestone in repo['milestones']:
         if milestone['due_on'] is not None:
-            milestone['due_on'] = datetime.strptime(
-                    milestone['due_on'], "%Y-%m-%dT%XZ")
+            milestone['due_on'] = date_from_iso(milestone['due_on'])
             milestone['repo'] = repo_name
+            total = milestone['closed_issues'] + milestone['open_issues']
             milestone['progress'] = (
-                milestone['closed_issues'] /
-                ((milestone['closed_issues'] +
-                    milestone['open_issues']) or float('inf')) * 100)
-    cache.get('repos')[repo_name] = repo
+                milestone['closed_issues'] / (total or float('inf')))
+    cache['repos'][repo_name] = repo
 
 
 @app.route('/show/<display>')
 def show_now(display='year'):
-    start = datetime(datetime.now().year, datetime.now().month, 1)
-    if display == 'year':
-        stop = start + relativedelta(months=12)
-    if display == 'month':
-        stop = start + relativedelta(months=1)
-    return redirect(url_for(
-        'show', display=display,
-        start=datetime.strftime(start, "%Y-%m-%dT%XZ"),
-        stop=datetime.strftime(stop, "%Y-%m-%dT%XZ")))
-
+    today = date.today()
+    start = date(today.year, today.month, 1)
+    stop = start + relativedelta(months=(12 if display == 'year' else 1))
+    return redirect(url_for('show', display=display, start=start, stop=stop))
 
 
 @app.route('/show_month/<month_start>')
 def show_month(month_start):
-    stop = datetime.strptime(
-            month_start, "%Y-%m-%dT%XZ") + relativedelta(months=1)
+    stop = date_from_iso(month_start) + relativedelta(months=1)
     return redirect(url_for(
-        'show', display='month', start=month_start,
-        stop=datetime.strftime(stop, "%Y-%m-%dT%XZ")))
+        'show', display='month', start=month_start, stop=stop))
 
 
 @app.route('/show/<display>/<start>/<stop>')
 def show(display, start, stop):
-    display_step = {
-            'year': relativedelta(months=1),
-            'month': relativedelta(days=1)
-    }
-    date_start = datetime.strptime(start, "%Y-%m-%dT%XZ")
-    date_stop = datetime.strptime(stop, "%Y-%m-%dT%XZ")
+    display_step = (
+        relativedelta(months=1) if display == 'year'
+        else relativedelta(days=1))
 
-    repos_name = cache.get('users').get(session['username'])
+    repos_name = cache['users'].get(session['username'])
     if repos_name is None:
         return redirect(url_for('login'))
     milestones = []
@@ -131,28 +120,25 @@ def show(display, start, stop):
         if name not in cache.get('repos'):
             refresh_repo_milestones(name)
         milestones += cache.get('repos').get(name).get('milestones')
-
+    print(milestones)
     stones = get_stones_by_step(
-            milestones, date_start, date_stop, display_step.get(display))
+        milestones, date_from_iso(start), date_from_iso(stop), display_step)
     return render_template(
-            "{0}.jinja2".format(display), start=date_start,
-            stones_by_step=stones)
+        '{0}.jinja2'.format(display), start=date_from_iso(start),
+        stones_by_step=stones)
 
 
 def get_stones_by_step(all_stones, start, end, step):
-    result = []
     current = start
+    result = []
     while current < end:
         stones = [
             stone for stone in all_stones
-            if (stone['due_on'] and
-                current < stone['due_on'] < current + step)]
+            if stone['due_on'] and current <= stone['due_on'] < current + step]
         result.append((current, stones))
-        current = current + step
+        current += step
     return result
 
-
-app.secret_key = 'secret: chut'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
