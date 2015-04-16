026ebd17b6b80bafb00b6338fe2ff0f934260de1
Yohann Rebattu <yohann.rebattu@kozea.fr>
2015-04-16 15:59:23 +0200

    utilisation d'un décorateur pour le login

--- gibolt.py
+++ gibolt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from datetime import date
+from functools import wraps
 
 from dateutil.relativedelta import relativedelta
 from flask.ext.github import GitHub, GitHubError
@@ -47,19 +48,22 @@ def date_from_iso(iso_date):
     return date(*[int(value) for value in iso_date[:10].split('-')])
 
 
-@app.before_request
-def before_request():
-    if session.get('user'):
+def autologin(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        print('test')
+        if session.get('user') is None:
+            return redirect(url_for('login'))
         if cache['users'].get(session['user']) is None:
             cache['users'][session['user']] = get_allowed_repos()
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
+@autologin
 def index():
-    if session.get('user'):
-        return redirect(url_for('show_now', display='year'))
-    else:
-        return redirect(url_for('login'))
+    return redirect(url_for('show_now', display='year'))
 
 
 @app.route('/login', methods=('GET', 'POST'))
@@ -85,17 +89,17 @@ def authorized(oauth_token):
 
 def get_allowed_repos():
     repos = github.get(
-        'orgs/{0}/repos?type=all'.format(app.config['ORGANISATION']), all_pages=True)
+        'orgs/{0}/repos?type=all'.format(
+            app.config['ORGANISATION']), all_pages=True)
     return [repo['name'] for repo in repos]
 
 
 @app.route('/refresh/all')
+@autologin
 def refresh_all():
     repo_names = cache.get('users').get(session['user'])
     for repo_name in repo_names:
         refresh_repo_milestones(repo_name)
-    url = 'orgs/{0}/issues?filter=all&state=all&labels=sprint'.format(
-            app.config['ORGANISATION'])
     flash('cache is fresh as a peppermint candy')
     return redirect(url_for('index'))
 
@@ -116,28 +120,48 @@ def refresh_repo_milestones(repo_name):
 
 
 @app.route('/issues/sprint')
+@autologin
 def show_sprint_issues():
     filters = {'filter': 'all', 'state': 'all', 'labels': 'sprint'}
     return redirect(url_for('show_issues', **filters))
 
+
 @app.route('/issues')
+@autologin
 def show_issues():
     filters = request.args
     url = 'orgs/{0}/issues'.format(
             app.config['ORGANISATION'])
-    issues = github.get(url, all_pages=True)
-    end_url='?'
+    end_url = '?'
+    query = ''
     for key, value in filters.items():
         end_url += "{0}={1}&".format(key, value)
+        query += "{0}:{1} ".format(key, value)
     end_url = end_url[:-1]
     issues = github.get(url + end_url, all_pages=True)
     opened = len([issue for issue in issues if issue['state'] == 'open'])
-    closed = len([issue for issue in issues if issue['state'] == 'closed']) or float('inf')
+    closed = len([issue for issue in issues if issue['state'] == 'closed'])
     return render_template(
-        'issues.jinja2', issues=issues, opened=opened, closed=closed)
+        'issues.jinja2', issues=issues,
+        opened=opened, closed=closed, query=query)
+
+
+@app.route('/issues/custom_query')
+@autologin
+def show_issues_query():
+    # parse query to filter here
+    query = request.args.get('query').strip()
+    args = query.split(' ')
+    filters = {}
+    for arg in args:
+        if ':' in arg:
+            key, value = arg.split(':')
+            filters[key] = value
+    return redirect(url_for('show_issues', **filters))
 
 
 @app.route('/stones/<display>')
+@autologin
 def show_now(display='year'):
     today = date.today()
     start = date(today.year, today.month, 1)
@@ -146,6 +170,7 @@ def show_now(display='year'):
 
 
 @app.route('/stones/month/<month_start>')
+@autologin
 def show_month(month_start):
     stop = date_from_iso(month_start) + relativedelta(months=1)
     return redirect(url_for(
@@ -153,6 +178,7 @@ def show_month(month_start):
 
 
 @app.route('/stones/<display>/<start>/<stop>')
+@autologin
 def show(display, start, stop):
     display_step = (
         relativedelta(months=1) if display == 'year'
