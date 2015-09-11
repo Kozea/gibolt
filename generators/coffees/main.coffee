3b96cb2e3d9a5b4bd12807f4b7a3ad3972b38d69
Yohann Rebattu <yohann.rebattu@kozea.fr>
2015-09-11 16:43:03 +0200

    allow check by group

--- generators/coffees/main.coffee
+++ generators/coffees/main.coffee
@@ -1,9 +1,9 @@
-toggle = ()->
-  aInputs = document.getElementsByName('issues')
-  oInputs = document.getElementById('chk_all')
+root = exports ? this
+
+root.toggle = (oInput, input_class)->
+  aInputs = document.getElementsByClassName(input_class)
   for input in aInputs
-    input.checked = oInputs.checked
+    input.checked = oInput.checked
 
 document.addEventListener 'DOMContentLoaded', () ->
   document.getElementById("useless-submit").hidden = true
-  document.getElementById("chk_all").onclick = toggle
--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,21 @@
 (function() {
-  var toggle;
+  var root;
 
-  toggle = function() {
-    var aInputs, i, input, len, oInputs, results;
-    aInputs = document.getElementsByName('issues');
-    oInputs = document.getElementById('chk_all');
+  root = typeof exports !== "undefined" && exports !== null ? exports : this;
+
+  root.toggle = function(oInput, input_class) {
+    var aInputs, i, input, len, results;
+    aInputs = document.getElementsByClassName(input_class);
     results = [];
     for (i = 0, len = aInputs.length; i < len; i++) {
       input = aInputs[i];
-      results.push(input.checked = oInputs.checked);
+      results.push(input.checked = oInput.checked);
     }
     return results;
   };
 
   document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("useless-submit").hidden = true;
-    return document.getElementById("chk_all").onclick = toggle;
+    return document.getElementById("useless-submit").hidden = true;
   });
 
 }).call(this);
