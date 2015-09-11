c9134dc93140da0dfeab1a8b398313c09031834a
Yohann Rebattu <yohann.rebattu@kozea.fr>
2015-09-11 15:45:05 +0200

    add check all box function

--- generators/coffees/main.coffee
+++ generators/coffees/main.coffee
@@ -1,2 +1,9 @@
+toggle = ()->
+  aInputs = document.getElementsByName('issues')
+  oInputs = document.getElementById('chk_all')
+  for input in aInputs
+    input.checked = oInputs.checked
+
 document.addEventListener 'DOMContentLoaded', () ->
   document.getElementById("useless-submit").hidden = true
+  document.getElementById("chk_all").onclick = toggle
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,21 @@
 (function() {
+  var toggle;
+
+  toggle = function() {
+    var aInputs, i, input, len, oInputs, results;
+    aInputs = document.getElementsByName('issues');
+    oInputs = document.getElementById('chk_all');
+    results = [];
+    for (i = 0, len = aInputs.length; i < len; i++) {
+      input = aInputs[i];
+      results.push(input.checked = oInputs.checked);
+    }
+    return results;
+  };
+
   document.addEventListener('DOMContentLoaded', function() {
-    return document.getElementById("useless-submit").hidden = true;
+    document.getElementById("useless-submit").hidden = true;
+    return document.getElementById("chk_all").onclick = toggle;
   });
 
 }).call(this);
