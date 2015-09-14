cc98ad73fcb323aa32f5cdd04030b29c938438fe
Yohann Rebattu <yohann.rebattu@kozea.fr>
2015-09-14 11:52:53 +0200

    coherent checkboxes state

--- generators/coffees/main.coffee
+++ generators/coffees/main.coffee
@@ -5,5 +5,44 @@ root.toggle = (oInput, input_class)->
   for input in aInputs
     input.checked = oInput.checked
 
+root.toggleparent = (oInput, parent_class)->
+  parents = document.getElementsByClassName(parent_class + ' parent')
+  brothers = document.getElementsByClassName(oInput.className)
+  check = 0
+  uncheck = 0
+  for brother in brothers
+    if brother.className.indexOf('parent') == -1
+      if brother.checked
+        check += 1
+      else
+        uncheck += 1
+  for parent in parents
+    if check == 0
+      parent.checked = false
+      parent.indeterminate = false
+    else if uncheck == 0
+      parent.checked = true
+      parent.indeterminate = false
+    else
+      parent.indeterminate = true
+  cousins = document.getElementsByClassName('issues')
+  check = 0
+  uncheck = 0
+  for cousin in cousins
+    if cousin.className.indexOf('parent') == -1
+      if cousin.checked
+        check += 1
+      else
+        uncheck += 1
+  gaia = document.getElementById('gaia')
+  if check == 0
+    gaia.checked = false
+    gaia.indeterminate = false
+  else if uncheck == 0
+    gaia.checked = true
+    gaia.indeterminate = false
+  else
+    gaia.indeterminate = true
+
 document.addEventListener 'DOMContentLoaded', () ->
   document.getElementById("useless-submit").hidden = true
--- static/js/main.js
+++ static/js/main.js
@@ -14,6 +14,59 @@
     return results;
   };
 
+  root.toggleparent = function(oInput, parent_class) {
+    var brother, brothers, check, cousin, cousins, gaia, i, j, k, len, len1, len2, parent, parents, uncheck;
+    parents = document.getElementsByClassName(parent_class + ' parent');
+    brothers = document.getElementsByClassName(oInput.className);
+    check = 0;
+    uncheck = 0;
+    for (i = 0, len = brothers.length; i < len; i++) {
+      brother = brothers[i];
+      if (brother.className.indexOf('parent') === -1) {
+        if (brother.checked) {
+          check += 1;
+        } else {
+          uncheck += 1;
+        }
+      }
+    }
+    for (j = 0, len1 = parents.length; j < len1; j++) {
+      parent = parents[j];
+      if (check === 0) {
+        parent.checked = false;
+        parent.indeterminate = false;
+      } else if (uncheck === 0) {
+        parent.checked = true;
+        parent.indeterminate = false;
+      } else {
+        parent.indeterminate = true;
+      }
+    }
+    cousins = document.getElementsByClassName('issues');
+    check = 0;
+    uncheck = 0;
+    for (k = 0, len2 = cousins.length; k < len2; k++) {
+      cousin = cousins[k];
+      if (cousin.className.indexOf('parent') === -1) {
+        if (cousin.checked) {
+          check += 1;
+        } else {
+          uncheck += 1;
+        }
+      }
+    }
+    gaia = document.getElementById('gaia');
+    if (check === 0) {
+      gaia.checked = false;
+      return gaia.indeterminate = false;
+    } else if (uncheck === 0) {
+      gaia.checked = true;
+      return gaia.indeterminate = false;
+    } else {
+      return gaia.indeterminate = true;
+    }
+  };
+
   document.addEventListener('DOMContentLoaded', function() {
     return document.getElementById("useless-submit").hidden = true;
   });
