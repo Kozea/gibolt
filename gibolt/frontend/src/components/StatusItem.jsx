2dc5e9117f01ff0cce392e63b5aab287bd5cd9fe
Florian Mounier <florian.mounier@kozea.fr>
2016-11-09 16:28:06 +0100

    Fix status for state

--- gibolt/frontend/src/components/StatusItem.jsx
+++ gibolt/frontend/src/components/StatusItem.jsx
@@ -6,7 +6,7 @@ import './StatusItem.sass'
 const b = block('StatusItem')
 export default function StatusItem(props) {
   return (
-    <li className={ b({status: props.active}) }>
+    <li className={ b({active: props.active}) }>
       <a className={ b('link') } href={props.action}>{props.children}</a>
     </li>
   )
