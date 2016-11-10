bdba12be14f92d4cc056d4ab83bb45a48e6cfbe7
Yohann Rebattu <yohann.rebattu@kozea.fr>
2016-11-10 10:52:01 +0100

    add reducer

--- gibolt/frontend/src/reducers/index.js
+++ gibolt/frontend/src/reducers/index.js
@@ -1,52 +1,17 @@
 import { combineReducers } from 'redux'
-const initialState = {
-  val: 0,
-  start: 0,
-  step: 1
-}
 
-const count = (state=initialState, action) => {
-  switch (action.type) {
-    case 'INCREMENT_COUNTER':
-      return Object.assign({}, state, {
-        val: state.val += state.step
-      })
-    case 'RESET_COUNTER':
-      return Object.assign({}, state, {
-        val: state.start
-      })
-    default:
-      return state
-  }
-}
 
-const logs = (state=[], action) => {
+const reduceIssuesState = (state='open', action) => {
   switch (action.type) {
-    case 'LOG':
-      return [
-        action.message,
-        ...state
-      ]
-    case 'DELETE_LOG':
-      return state.filter(e => e.id != action.messageId)
-    default:
-      return state
-  }
-}
-
-const version = (state='?', action) => {
-  switch (action.type) {
-    case 'SET_VERSION':
-      return action.version
+    case 'SET_ISSUES_STATE':
+      return action.issuesState
     default:
       return state
   }
 }
 
 const reducer = combineReducers({
-  count,
-  messages: logs,
-  version: version
+    issuesState: reduceIssuesState
 })
 
 export default reducer
