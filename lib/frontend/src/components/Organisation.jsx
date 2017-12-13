ccaca864b9365b9e1b96b86ec35c675f78c6a242
rojinpolat <rojin.polat69@gmail.com>
2017-12-13 12:00:03 +0100

    Organisation: list_circles OK

--- lib/frontend/src/components/Organisation.jsx
+++ lib/frontend/src/components/Organisation.jsx
@@ -4,26 +4,30 @@ import { Helmet } from 'react-helmet'
 import { connect } from '../utils'
 import Loading from './Loading'
 
-function Organisation(loading, error, circle) {
+function Organisation(
+  loading,
+  error
+  // circles
+) {
   return (
     <div>
       <Helmet>
         <title>Gibolt - Organisation</title>
       </Helmet>
       {loading && <Loading />}
-      {error && (
+      {/* {error && (
         <article>
           <h2>Error during circles fetch</h2>
           <code>{error}</code>
         </article>
-      )}
+      )} */}
       Organisation
-      {(console.log('circles'), console.log(circle))}
+      {/* {(console.log('circles'), console.log(circles))} */}
     </div>
   )
 }
 export default connect(state => ({
-  circle: state.organisaton.results.circle,
-  loading: state.organisaton.loading,
-  error: state.organisaton.error,
+  // circles: state.organisation.results.circles,
+  loading: state.organisation.loading,
+  error: state.organisation.error,
 }))(Organisation)
