2599a46e798c3898f8298463c750c59f56883bbb
rojinpolat <rojin.polat69@gmail.com>
2017-12-13 18:03:41 +0100

    Organisation: ajout des affichages de llistes et màj des cercles

--- lib/frontend/src/components/Organisation.jsx
+++ lib/frontend/src/components/Organisation.jsx
@@ -3,19 +3,20 @@ import { Helmet } from 'react-helmet'
 
 import { connect } from '../utils'
 // import Loading from './Loading'
-import { createCircle } from '../actions'
+// import { createCircle } from '../actions'
 
-function Organisation(loading, error, circles, onCreateCircle) {
+function Organisation(
+  loading,
+  error,
+  circles
+  // onCreateCircle
+) {
   return (
     <div>
       <Helmet>
         <title>Gibolt - Organisation</title>
       </Helmet>
       {/* {loading && <Loading />} */}
-      {/* Cercles existants:
-      {circles.map(circle => (
-        <li key={circle.circle_id}>{circle.circle_name}</li>
-      ))} */}
       {/* {error && (
         <article>
           <h2>Error during circles fetch</h2>
@@ -24,35 +25,63 @@ function Organisation(loading, error, circles, onCreateCircle) {
       )} */}
       {/* {circles.map(circle => (
         <li key={circle.circle_id}> */}
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          onCreateCircle(
-            e.target.name.value,
-            e.target.purpose.value,
-            e.target.domain.value,
-            e.target.accountabilities.value
-          )
-        }}
-      >
-        <label>Nom du cercle:</label>
-        <input required name="name" />
-        <br />
-        <br />
-        <label>Objectif(s):</label>
-        <input required name="purpose" />
-        <br />
-        <br />
-        <label>Domaine:</label>
-        <input required name="domain" />
-        <br />
-        <br />
-        <label>Redevabilités:</label>
-        <input required name="accountabilities" />
-        <br />
-        <br />
-        <button type="submit">Créer un cercle</button>
-      </form>
+      <article>
+        <h2>CREER UN NOUVEAU CERCLE :</h2>
+        <form
+        // onSubmit={e => {
+        //   e.preventDefault()
+        //   onCreateCircle(
+        //     e.target.name.value,
+        //     e.target.purpose.value,
+        //     e.target.domain.value,
+        //     e.target.accountabilities.value
+        //   )
+        // }}
+        >
+          <label>Nom du cercle:</label>
+          <input required name="name" />
+          <br />
+          <br />
+          <label>Objectif(s):</label>
+          <input required name="purpose" />
+          <br />
+          <br />
+          <label>Domaine:</label>
+          <input required name="domain" />
+          <br />
+          <br />
+          <label>Redevabilités:</label>
+          <input required name="accountabilities" />
+          <br />
+          <br />
+          <button type="submit">Créer un cercle</button>
+        </form>
+      </article>
+      <br />
+      <article>
+        <ul>
+          <h2>LISTE DES CERCLES :</h2>
+          {/* {circles.map(circle => (
+            <li key={circle.circle_id}>{circle.circle_name}</li>
+          ))} */}
+        </ul>
+      </article>
+      <article>
+        <ul>
+          <h2>MÀJ UN CERCLE :</h2>
+          <label>Cercle: </label>
+          <select>
+            <option>Valeur 1</option>
+            <option>Valeur 2</option>
+            <option>Valeur 3</option>
+          </select>
+          <br />
+          <button type="submit"> Modifier</button>
+          {/* {circles.map(circle => (
+            <li key={circle.circle_id}>{circle.circle_name}</li>
+          ))} */}
+        </ul>
+      </article>
       {/* </li>
       ))} */}
       {(console.log('circles'), console.log(circles))}
@@ -64,10 +93,10 @@ export default connect(
     circles: state.organisation.results.circles,
     loading: state.organisation.loading,
     error: state.organisation.error,
-  }),
-  dispatch => ({
-    onCreateCircle: (name, purpose, domain, accountabilities) => {
-      dispatch(createCircle(name, purpose, domain, accountabilities))
-    },
   })
+  // , dispatch => ({
+  //   onCreateCircle: (name, purpose, domain, accountabilities) => {
+  //     dispatch(createCircle(name, purpose, domain, accountabilities))
+  //   },
+  // })
 )(Organisation)
