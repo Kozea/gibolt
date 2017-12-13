cbfd29d5c30dd355e59d1900ffa88a4859051fc8
Sam <sam@samr1.net>
2017-12-13 17:55:42 +0100

    new database model - see #52

--- lib/backend/api/gibolt.py
+++ lib/backend/api/gibolt.py
@@ -1,17 +1,41 @@
 from unrest import UnRest
 
 from .. import app, session_unrest
-from .models import Circle, Report, Role
+from ..routes.auth import needlogin
+from .models import Circle, Item, Milestone_circle, Report, Role
 
 # Declare rest endpoints for gibolt Database
 rest = UnRest(app, session_unrest)
+
 rest(
     Circle,
     methods=['GET', 'PUT', 'POST', 'DELETE'],
     relationships={
-        'roles': rest(Role, only=['role_id']),
+        'roles': rest(Role, only=['role_id', 'user_id']),
     },
     name='circles'
 )
-rest(Role, methods=['GET', 'PUT', 'POST', 'DELETE'], name='roles')
-rest(Report, methods=['GET', 'PUT', 'POST', 'DELETE'], name='reports')
+
+rest(
+    Role,
+    methods=['GET', 'PUT', 'POST', 'DELETE'],
+    name='roles',
+    auth=needlogin)
+
+rest(
+    Report,
+    methods=['GET', 'PUT', 'POST', 'DELETE'],
+    name='reports',
+    auth=needlogin)
+
+rest(
+    Item,
+    methods=['GET', 'PUT', 'POST', 'DELETE'],
+    name='items',
+    auth=needlogin)
+
+rest(
+    Milestone_circle,
+    methods=['GET', 'PUT', 'POST', 'DELETE'],
+    name='milestones_circles',
+    auth=needlogin)
--- lib/backend/api/models.py
+++ lib/backend/api/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import backref, relationship
 
 Base = declarative_base()
 
@@ -12,10 +12,17 @@ class Circle(Base):
         primary_key=True,
         autoincrement=True,
         nullable=False)
+    parent_circle_id = Column(
+        Integer,
+        ForeignKey('circle.circle_id'),
+        nullable=True)
     circle_name = Column(String, unique=True)
     circle_purpose = Column(String)
     circle_domain = Column(String)
     circle_accountabilities = Column(String)
+    circle_children = relationship('Circle',
+                                   backref=backref('parent',
+                                                   remote_side=[circle_id]))
 
 
 class Role(Base):
@@ -25,10 +32,11 @@ class Role(Base):
         primary_key=True,
         autoincrement=True,
         nullable=False)
-    user_id = Column(Integer)
     circle_id = Column(
-        String,
-        ForeignKey('circle.circle_id'))
+        Integer,
+        ForeignKey('circle.circle_id'),
+        nullable=False)
+    user_id = Column(Integer)
     role_name = Column(String)
     role_purpose = Column(String)
     role_domain = Column(String)
@@ -37,15 +45,45 @@ class Role(Base):
     circle = relationship(Circle, backref='roles')
 
 
+class Item(Base):
+    __tablename__ = 'item'
+    item_id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False)
+    role_id = Column(
+        Integer,
+        ForeignKey('role.role_id'),
+        nullable=False)
+    item_type = Column(String)
+    content = Column(Text)
+    role = relationship(Role, backref='items')
+
+
 class Report(Base):
     __tablename__ = 'report'
     report_id = Column(
         Integer,
         primary_key=True,
         autoincrement=True,
-        nullable=True)
+        nullable=False)
     circle_id = Column(
-        String,
-        ForeignKey('circle.circle_id'))
-    type = Column(String)
+        Integer,
+        ForeignKey('circle.circle_id'),
+        nullable=False)
+    report_type = Column(String)
     created_at = Column(DateTime)
+    author_id = Column(Integer)
+    content = Column(Text)
+
+
+class Milestone_circle(Base):
+    __tablename__ = 'milestone_circle'
+    circle_id = Column(
+        Integer,
+        ForeignKey('circle.circle_id'),
+        primary_key=True)
+    milestone_id = Column(
+        Integer,
+        primary_key=True)
