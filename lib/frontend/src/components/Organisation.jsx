1f1fe7c7a3f6e4261fc89831700723d29f4e4258
rojinpolat <rojin.polat69@gmail.com>
2017-12-15 17:02:20 +0100

    Organisation: updateCircle OK

--- lib/frontend/src/components/Organisation.jsx
+++ lib/frontend/src/components/Organisation.jsx
@@ -3,9 +3,9 @@ import { Helmet } from 'react-helmet'
 
 import { connect } from '../utils'
 import Loading from './Loading'
-import { createCircle, deleteCircle } from '../actions'
+import { createCircle, deleteCircle, updateCircle } from '../actions'
 
-function Organisation({ error, loading, onSubmit, circles, btnClick }) {
+function Organisation({ error, loading, onSubmit, circles, btnClick, onEdit }) {
   return (
     <div>
       <Helmet>
@@ -70,6 +70,52 @@ function Organisation({ error, loading, onSubmit, circles, btnClick }) {
                   Supprimer
                 </button>
                 <button>Créer les roles</button>
+                <article>
+                  <h2>CREER UN NOUVEAU CERCLE :</h2>
+                  <form
+                    onSubmit={e => {
+                      onEdit(circle.circle_id, e)
+                    }}
+                  >
+                    <label>
+                      Nom :
+                      <input
+                        name="circle_name"
+                        defaultValue={circle.circle_name}
+                        required
+                      />
+                    </label>
+                    <br />
+                    <label>
+                      Objectif :
+                      <input
+                        name="circle_purpose"
+                        defaultValue={circle.circle_purpose}
+                        required
+                      />
+                    </label>
+                    <br />
+                    <label>
+                      Domaine :
+                      <input
+                        name="circle_domain"
+                        defaultValue={circle.circle_domain}
+                        required
+                      />
+                    </label>
+                    <br />
+                    <label>
+                      Redevabilités :
+                      <input
+                        name="circle_accountabilities"
+                        defaultValue={circle.circle_accountabilities}
+                        required
+                      />
+                    </label>
+                    <br />
+                    <input type="submit" value="Modifier le cercle" />
+                  </form>
+                </article>
               </li>
             ))}
           </ul>
@@ -98,6 +144,21 @@ export default connect(
         }, {})
       dispatch(createCircle(formCircle))
     },
-    btnClick: data => dispatch(deleteCircle(data)),
+    btnClick: data => {
+      return dispatch(deleteCircle(data))
+    },
+    onEdit: (id, e) => {
+      // e.preventDefault()
+      const formCircle = [].slice
+        .call(e.target.elements)
+        .reduce(function(map, obj) {
+          if (obj.name) {
+            map[obj.name] = obj.value
+          }
+
+          return map
+        }, {})
+      dispatch(updateCircle(id, formCircle))
+    },
   })
 )(Organisation)
