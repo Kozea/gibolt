4bc557eadd327433e0236aebd574ff8bfb042cd8
Sam <sam@samr1.net>
2017-12-21 09:29:31 +0100

    Issue creation: replace the pure function w/ a class

--- lib/frontend/src/components/IssueCreationDetail.jsx
+++ lib/frontend/src/components/IssueCreationDetail.jsx
@@ -15,135 +15,140 @@ import Loading from './Loading'
 
 const b = block('IssueCreationDetail')
 
-function IssueCreationDetail({
-  circles,
-  error,
-  issueForm,
-  labels,
-  loading,
-  onCircleChange,
-  onGoBack,
-  onProjectChange,
-  onTitleChange,
-  onSubmit,
-  repositories,
-}) {
-  const sortedRepos = sortRepos(repositories)
-  return (
-    <section className={b()}>
-      <Helmet>
-        <title>Gibolt - Create an issue</title>
-      </Helmet>
-      <h2>Create an issue</h2>
-      {loading && <Loading />}
-      {error && (
-        <article className={b('date', { error: true })}>
-          <h3>Error during issue creation</h3>
-          <code>{error}</code>
+class IssueCreationDetail extends React.Component {
+  componentDidMount() {}
+  render() {
+    const {
+      circles,
+      error,
+      issueForm,
+      labels,
+      loading,
+      onCircleChange,
+      onGoBack,
+      onProjectChange,
+      onTitleChange,
+      onSubmit,
+      repositories,
+    } = this.props
+    const sortedRepos = sortRepos(repositories)
+    return (
+      <section className={b()}>
+        <Helmet>
+          <title>Gibolt - Create an issue</title>
+        </Helmet>
+        <h2>Create an issue</h2>
+        {loading && <Loading />}
+        {error && (
+          <article className={b('date', { error: true })}>
+            <h3>Error during issue creation</h3>
+            <code>{error}</code>
+            <br />
+            <br />
+          </article>
+        )}
+        <form onSubmit={event => event.preventDefault()}>
+          <label>
+            Project (mandatory):
+            <select
+              id="project"
+              name="project"
+              onChange={event => onProjectChange(event.target.value)}
+            >
+              <option value="">{''}</option>
+              {sortedRepos.map(repo => (
+                <option key={repo.repo_id} value={repo.repo_name}>
+                  {repo.repo_name}
+                </option>
+              ))}
+            </select>
+          </label>
           <br />
+          <label>
+            Milestone:{' '}
+            <select id="milestone" name="milestone">
+              <option value="">{''}</option>
+              {issueForm.milestonesSelect.map(milestone => (
+                <option
+                  key={milestone.milestone_id}
+                  value={milestone.milestone_number}
+                >
+                  {milestone.milestone_title}
+                </option>
+              ))}
+            </select>
+          </label>
           <br />
-        </article>
-      )}
-      <form onSubmit={event => event.preventDefault()}>
-        <label>
-          Project (mandatory):
-          <select
-            id="project"
-            name="project"
-            onChange={event => onProjectChange(event.target.value)}
-          >
-            <option value="">{''}</option>
-            {sortedRepos.map(repo => (
-              <option key={repo.repo_id} value={repo.repo_name}>
-                {repo.repo_name}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <label>
-          Milestone:{' '}
-          <select id="milestone" name="milestone">
-            <option value="">{''}</option>
-            {issueForm.milestonesSelect.map(milestone => (
-              <option
-                key={milestone.milestone_id}
-                value={milestone.milestone_number}
-              >
-                {milestone.milestone_title}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <label>
-          Circle:{' '}
-          <select
-            id="circle"
-            name="circle"
-            onChange={event => onCircleChange(event.target.value)}
-          >
-            <option value="">{''}</option>
-            {circles.map(circle => (
-              <option key={circle.circle_id} value={circle.circle_id}>
-                {circle.circle_name}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <label>
-          Role assignment:
-          <select id="roles" name="roles">
-            <option value="">{''}</option>
-            {issueForm.rolesSelect.map(role => (
-              <option key={role.role_id} value={role.user_id}>
-                {role.role_name}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <label>
-          Priority:{' '}
-          <select id="priority" name="priority">
-            {labels.map(label => (
-              <option key={label.text} value={label.text}>
-                {label.text}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <label>
-          Title (mandatory):<br />
-          <input
-            id="title"
-            name="title"
-            onChange={event => onTitleChange(event)}
-          />
-        </label>
-        <br />
-        <label>
-          Description: <textarea id="body" name="body" rows="7" />
-        </label>
-        <br />
-        <article className={b('action')}>
-          <button
-            type="submit"
-            onClick={event => onSubmit(event)}
-            disabled={issueForm.project === '' || issueForm.title === ''}
-          >
-            Create
-          </button>
-          <button type="submit" onClick={() => onGoBack()}>
-            Cancel
-          </button>
-        </article>
-      </form>
-    </section>
-  )
+          <label>
+            Circle:{' '}
+            <select
+              id="circle"
+              name="circle"
+              onChange={event => onCircleChange(event.target.value)}
+            >
+              <option value="">{''}</option>
+              {circles.map(circle => (
+                <option key={circle.circle_id} value={circle.circle_id}>
+                  {circle.circle_name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <br />
+          <label>
+            Role assignment:
+            <select id="roles" name="roles">
+              <option value="">{''}</option>
+              {issueForm.rolesSelect.map(role => (
+                <option key={role.role_id} value={role.user_id}>
+                  {role.role_name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <br />
+          <label>
+            Priority:{' '}
+            <select id="priority" name="priority">
+              {labels.map(label => (
+                <option key={label.text} value={label.text}>
+                  {label.text}
+                </option>
+              ))}
+            </select>
+          </label>
+          <br />
+          <label>
+            Title (mandatory):<br />
+            <input
+              id="title"
+              name="title"
+              onChange={event => onTitleChange(event)}
+            />
+          </label>
+          <br />
+          <label>
+            Description: <textarea id="body" name="body" rows="7" />
+          </label>
+          <br />
+          <article className={b('action')}>
+            <button
+              type="submit"
+              onClick={event => onSubmit(event)}
+              disabled={issueForm.project === '' || issueForm.title === ''}
+            >
+              Create
+            </button>
+            <button type="submit" onClick={() => onGoBack()}>
+              Cancel
+            </button>
+          </article>
+        </form>
+      </section>
+    )
+  }
 }
+
 export default connect(
   state => ({
     circles: state.circles.results,
