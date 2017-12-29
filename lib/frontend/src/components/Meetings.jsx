36f2c2186fc99f2bb615a498a74e7680f7216785
Sam <sam@samr1.net>
2017-12-29 16:55:57 +0100

    Meeting reports: selects for circle and meeting types

--- lib/frontend/src/components/Meetings.jsx
+++ lib/frontend/src/components/Meetings.jsx
@@ -8,26 +8,54 @@ import Loading from './Loading'
 
 const b = block('Meetings')
 
-function Meetings({ error, loading }) {
+function Meetings({ circles, meetingsTypes }) {
   return (
     <section className={b()}>
       <Helmet>
         <title>Gibolt - Meetings</title>
       </Helmet>
-      {error && (
+      {(circles.error || meetingsTypes.error) && (
         <article className={b('group', { error: true })}>
-          <h2>Error during issue fetch</h2>
-          <code>{error}</code>
+          <h2>Error during fetch</h2>
+          <code>
+            {circles.error
+              ? `circles : ${circles.error}`
+              : `Meetings types ${meetingsTypes.error}`}
+          </code>
         </article>
       )}
-      {loading && <Loading />}
+      {(circles.loading || meetingsTypes.loading) && <Loading />}
       <article className={b('meetings')}>
         <h2>Meetings</h2>
+        <form onSubmit={event => event.preventDefault()}>
+          <label>
+            Circle:
+            <select id="circles" name="circles">
+              <option value="">All</option>
+              {circles.results.map(circle => (
+                <option key={circle.circle_id} value={circle.circle_id}>
+                  {circle.circle_name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label>
+            Meetings type:
+            <select id="meeting-type" name="meeting-type">
+              <option value="">All</option>
+              {meetingsTypes.results.map(type => (
+                <option key={type.type_id} value={type.type_name}>
+                  {type.type_name}
+                </option>
+              ))}
+            </select>
+          </label>
+        </form>
       </article>
     </section>
   )
 }
 export default connect(state => ({
-  loading: state.circles.loading,
-  error: state.circles.errors,
+  circles: state.circles,
+  meetingsTypes: state.meetingsTypes,
 }))(Meetings)
