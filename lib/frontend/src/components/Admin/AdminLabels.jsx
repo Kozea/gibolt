7ebcb7fe78ab9dc4648d11521e8c5e49433c5a5b
Sam <sam@samr1.net>
2018-01-25 18:10:20 +0100

    Admin: adding new label (WIP)

--- lib/frontend/src/components/Admin/AdminLabels.jsx
+++ lib/frontend/src/components/Admin/AdminLabels.jsx
@@ -13,15 +13,42 @@ class Labels extends React.Component {
   componentDidMount() {
     this.props.sync()
   }
+  componentWillReceiveProps(nextProps) {
+    if (
+      this.props.adminLabels.label_types.length === 0 &&
+      nextProps.adminLabels.label_types.length > 0
+    ) {
+      this.props.onTypeChange(
+        nextProps.adminLabels.label_types[0].label_type_id
+      )
+    }
+  }
+
+  getPriorityLabelId(labelTypes) {
+    const priorityLabel = labelTypes.filter(
+      label => label.label_type_name === 'priority'
+    )
+    if (priorityLabel.length > 0) {
+      return priorityLabel[0].label_type_id
+    }
+    return null
+  }
 
   render() {
     const {
       adminLabels,
       error,
       loading,
+      onSubmit,
       onTypeChange,
-      selectedType,
+      selectedLabelTypeId,
     } = this.props
+    adminLabels.label_types.sort(function(a, c) {
+      return a.label_type_name > c.label_type_name
+        ? 1
+        : c.label_type_name > a.label_type_name ? -1 : 0
+    })
+    const priorityLabelId = this.getPriorityLabelId(adminLabels.label_types)
     return (
       <section className={b()}>
         <article className={b('adminLabels')}>
@@ -33,47 +60,77 @@ class Labels extends React.Component {
             </article>
           )}
           {loading && <Loading />}
-          <form>
+          <label>
+            Label types:
+            <select
+              id="labelTypes"
+              name="labelTypes"
+              onChange={event => onTypeChange(event.target.value)}
+              value={selectedLabelTypeId}
+            >
+              {adminLabels.label_types.map(labelTypes => (
+                <option
+                  key={labelTypes.label_type_id}
+                  value={labelTypes.label_type_id}
+                >
+                  {labelTypes.label_type_name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <br />
+          <br />
+          <label>Labels</label>
+          <ul>
+            {adminLabels.labels
+              .filter(label => label.label_type_id === selectedLabelTypeId)
+              .map(label => (
+                <li
+                  className={b('item')}
+                  key={label.label_id}
+                  style={{
+                    color: label.label_color,
+                  }}
+                >
+                  <span className={b('text')}>
+                    {label.label_name}
+                    {label.priorities[0] &&
+                      ` (priority: ${label.priorities[0].value})`}
+                    <i className="fa fa-edit btn" aria-hidden="true" />
+                    <i className="fa fa-trash btn" aria-hidden="true" />
+                  </span>
+                </li>
+              ))}
+          </ul>
+          <br />
+          Add a new label:
+          <form onSubmit={event => onSubmit(event, selectedLabelTypeId)}>
             <label>
-              Label types:
-              <select
-                id="labelTypes"
-                name="labelTypes"
-                onChange={event => onTypeChange(event.target.value)}
-                value={selectedType}
-              >
-                <option value="" />
-                {adminLabels.label_types.map(labelTypes => (
-                  <option
-                    key={labelTypes.label_type_id}
-                    value={labelTypes.label_type_id}
-                  >
-                    {labelTypes.label_type_name}
-                  </option>
-                ))}
-              </select>
+              Name <input id="newLabelName" name="newLabelName" required />
             </label>
-            <br />
-            <label>Labels:</label>
-            <ul>
-              {adminLabels.labels
-                .filter(label => label.label_type_id === selectedType)
-                .map(label => (
-                  <li
-                    className={b('item')}
-                    key={label.label_id}
-                    style={{
-                      color: label.label_color,
-                    }}
-                  >
-                    <span className={b('text')}>
-                      {label.label_name}
-                      {label.priorities[0] &&
-                        ` (priority: ${label.priorities[0].value})`}
-                    </span>
-                  </li>
-                ))}
-            </ul>
+            <label>
+              Color:{' '}
+              <input
+                id="newLabelColor"
+                name="newLabelColor"
+                type="color"
+                required
+              />
+            </label>
+            {selectedLabelTypeId === priorityLabelId && (
+              <label>
+                Priority:
+                <input
+                  id="newLabelPriority"
+                  name="newLabelPriority"
+                  required
+                  type="number"
+                />
+              </label>
+            )}
+            <article className={b('action')}>
+              <button type="submit">Add Label</button>
+            </article>
           </form>
         </article>
       </section>
@@ -86,11 +143,24 @@ export default connect(
     adminLabels: state.adminLabels.results,
     error: state.adminLabels.error,
     loading: state.adminLabels.loading,
-    selectedType: state.adminLabels.selectedType,
+    selectedLabelTypeId: state.adminLabels.selectedLabelTypeId,
   }),
   dispatch => ({
-    onTypeChange: labelTypeId => {
-      dispatch(updateSelectedLabelType(labelTypeId))
+    onSubmit: (event, selectedLabelTypeId) => {
+      event.preventDefault()
+      const NewLabel = {
+        label_name: event.target.newLabelName.value,
+        label_color: event.target.newLabelColor.value,
+      }
+      const NewPriorityValue = event.target.newLabelPriority
+        ? +event.target.newLabelPriority.value
+        : null
+      console.log(NewLabel)
+      console.log(NewPriorityValue)
+      console.log(selectedLabelTypeId)
+    },
+    onTypeChange: value => {
+      dispatch(updateSelectedLabelType(+value))
     },
     sync: () => {
       dispatch(setLoading('adminLabels'))
