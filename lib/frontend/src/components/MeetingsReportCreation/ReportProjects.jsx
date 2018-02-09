e73778b830a6fe5cc97630a037873d6190dffc03
Sam <sam@samr1.net>
2018-02-09 15:47:27 +0100

    Meeting report: refactor + last report date bugfix

---
+++ lib/frontend/src/components/MeetingsReportCreation/ReportProjects.jsx
@@ -0,0 +1,136 @@
+import './MeetingsReportCreation.sass'
+
+import { format } from 'date-fns'
+import { stringify } from 'query-string'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Progress from './../Progress'
+import { block } from '../../utils'
+
+const b = block('MeetingsReportCreation')
+
+export default function ReportProjects(props) {
+  const { circleMilestones, issues, onMilestoneClick } = props
+  return (
+    <span>
+      <h3>Projects:</h3>
+      <ul>
+        {circleMilestones.length > 0 &&
+          circleMilestones.map(milestone => (
+            <li key={milestone.milestone_number} title={milestone.description}>
+              <a
+                className={b('unlink')}
+                href={milestone.html_url}
+                target="_blank"
+              >
+                <span className={b(`bullet ${milestone.state}`)} />
+                {milestone.repo_name}
+                {' - '}
+                <span className={b('lab')}>{milestone.milestone_title}</span>
+              </a>
+              {' -'}
+              <Progress
+                val={milestone.closed_issues}
+                total={milestone.open_issues + milestone.closed_issues}
+              />
+              <span className={b('due-date')}>
+                {' ('}
+                {milestone.due_on
+                  ? `due on: ${format(
+                      new Date(milestone.due_on),
+                      'DD/MM/YYYY'
+                    )}`
+                  : 'no due date'}
+                {')'}
+              </span>
+              {milestone.state === 'open' && (
+                <Link
+                  className={b('unlink')}
+                  target="_blank"
+                  title="open an issue"
+                  to={{
+                    pathname: '/createIssue',
+                    search: stringify({
+                      grouper: 'milestone',
+                      group: `${milestone.repo_name} ⦔ ${
+                        milestone.milestone_number
+                      }`,
+                    }),
+                  }}
+                >
+                  <i
+                    className="fa fa-plus-circle addCircle"
+                    aria-hidden="true"
+                  />
+                </Link>
+              )}
+              {issues.length > 0 &&
+                issues.filter(
+                  issue => issue.milestone_id === milestone.milestone_id
+                ).length > 0 && (
+                  <span>
+                    <span
+                      className={b('lighter')}
+                      onClick={() => onMilestoneClick(milestone.milestone_id)}
+                    >
+                      show/hide closed issues since last report
+                    </span>
+                    {milestone.is_expanded && (
+                      <span>
+                        <br />
+                        <ul className={b('tickets')}>
+                          {issues
+                            .filter(
+                              issue =>
+                                issue.milestone_number ===
+                                  milestone.milestone_number &&
+                                !issue.pull_request
+                            )
+                            .map(issue => (
+                              <li key={issue.ticket_id} title={issue.body}>
+                                <span className={b('bullet')} />
+                                <a href={issue.html_url} target="_blank">
+                                  #{issue.ticket_number}
+                                </a>{' '}
+                                {issue.ticket_title},
+                                <span className={b('lighter')}>
+                                  closed:{' '}
+                                  {format(
+                                    new Date(issue.closed_at),
+                                    'DD/MM/YYYY HH:mm'
+                                  )}
+                                  , last update:{' '}
+                                  {format(
+                                    new Date(issue.updated_at),
+                                    'DD/MM/YYYY HH:mm'
+                                  )}
+                                </span>
+                                {issue.assignees.map(user => (
+                                  <img
+                                    key={user.user_id}
+                                    className={b('avatar')}
+                                    src={user.avatar_url}
+                                    alt="avatar"
+                                    title={user.user_name}
+                                  />
+                                ))}
+                              </li>
+                            ))}
+                        </ul>
+                      </span>
+                    )}
+                  </span>
+                )}
+              <br />
+              <input
+                className="largeInput"
+                id="milestones"
+                name={milestone.milestone_title}
+              />
+            </li>
+          ))}
+      </ul>
+    </span>
+  )
+}
--- lib/frontend/src/components/MeetingsReportCreation/index.jsx
+++ lib/frontend/src/components/MeetingsReportCreation/index.jsx
@@ -1,10 +1,9 @@
 import './MeetingsReportCreation.sass'
 
-import { format } from 'date-fns'
-import { parse, stringify } from 'query-string'
+import { parse } from 'query-string'
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { Link, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
 import {
   checkMarkdown,
@@ -27,7 +26,8 @@ import {
 import { block, connect, sortUsers } from '../../utils'
 import Loading from './../Loading'
 import MarkdownEditor from './../MarkdownEditor'
-import Progress from './../Progress'
+import ReportItems from './ReportItems'
+import ReportProjects from './ReportProjects'
 
 const b = block('MeetingsReportCreation')
 
@@ -220,199 +220,15 @@ class MeetingsReportCreation extends React.Component {
                   </span>
                   {params.meeting_name === 'Triage' && (
                     <span>
-                      {selectedCircle.roles &&
-                        selectedCircle.roles.length > 0 && (
-                          <span>
-                            <h3>Recurrent actions:</h3>
-                            <ul>
-                              {items &&
-                                items.map(
-                                  roleItems =>
-                                    roleItems.items &&
-                                    roleItems.items
-                                      .filter(
-                                        item => item.item_type === 'checklist'
-                                      )
-                                      .map(item => (
-                                        <li key={item.item_id}>
-                                          <input
-                                            type="checkbox"
-                                            name={`${roleItems.role_name} - ${
-                                              item.content
-                                            }`}
-                                            id="actions"
-                                          />
-                                          {roleItems.role_name} - {item.content}
-                                        </li>
-                                      ))
-                                )}
-                            </ul>
-                            <h3>Indicators:</h3>
-                            <ul>
-                              {items &&
-                                items.map(
-                                  roleItems =>
-                                    roleItems.items &&
-                                    roleItems.items
-                                      .filter(
-                                        item => item.item_type === 'indicator'
-                                      )
-                                      .map(item => (
-                                        <li key={item.item_id}>
-                                          <span className={b('bullet')} />
-                                          {roleItems.role_name} - {item.content}
-                                          :{' '}
-                                          <input
-                                            type="text"
-                                            name={`${roleItems.role_name} - ${
-                                              item.content
-                                            }`}
-                                            id="indicateurs"
-                                            className="smallInput"
-                                          />
-                                        </li>
-                                      ))
-                                )}
-                            </ul>
-                          </span>
-                        )}
-                      <h3>Projects:</h3>
-                      <ul>
-                        {circleMilestones.length > 0 &&
-                          circleMilestones.map(milestone => (
-                            <li
-                              key={milestone.milestone_number}
-                              title={milestone.description}
-                            >
-                              <a
-                                className={b('unlink')}
-                                href={milestone.html_url}
-                                target="_blank"
-                              >
-                                <span
-                                  className={b(`bullet ${milestone.state}`)}
-                                />
-                                {milestone.repo_name}
-                                {' - '}
-                                <span className={b('lab')}>
-                                  {milestone.milestone_title}
-                                </span>
-                              </a>
-                              {' -'}
-                              <Progress
-                                val={milestone.closed_issues}
-                                total={
-                                  milestone.open_issues +
-                                  milestone.closed_issues
-                                }
-                              />
-                              <span className={b('due-date')}>
-                                {' ('}
-                                {milestone.due_on
-                                  ? `due on: ${format(
-                                      new Date(milestone.due_on),
-                                      'DD/MM/YYYY'
-                                    )}`
-                                  : 'no due date'}
-                                {')'}
-                              </span>
-                              {milestone.state === 'open' && (
-                                <Link
-                                  className={b('unlink')}
-                                  target="_blank"
-                                  title="open an issue"
-                                  to={{
-                                    pathname: '/createIssue',
-                                    search: stringify({
-                                      grouper: 'milestone',
-                                      group: `${milestone.repo_name} ⦔ ${
-                                        milestone.milestone_number
-                                      }`,
-                                    }),
-                                  }}
-                                >
-                                  <i
-                                    className="fa fa-plus-circle addCircle"
-                                    aria-hidden="true"
-                                  />
-                                </Link>
-                              )}
-                              {issues.length > 0 &&
-                                issues.filter(
-                                  issue =>
-                                    issue.milestone_id ===
-                                    milestone.milestone_id
-                                ).length > 0 && (
-                                  <span>
-                                    <span
-                                      className={b('lighter')}
-                                      onClick={() =>
-                                        onMilestoneClick(milestone.milestone_id)
-                                      }
-                                    >
-                                      show/hide closed issues since last report
-                                    </span>
-                                    {milestone.is_expanded && (
-                                      <span>
-                                        <br />
-                                        <ul className={b('tickets')}>
-                                          {issues
-                                            .filter(
-                                              issue =>
-                                                issue.milestone_number ===
-                                                  milestone.milestone_number &&
-                                                !issue.pull_request
-                                            )
-                                            .map(issue => (
-                                              <li
-                                                key={issue.ticket_id}
-                                                title={issue.body}
-                                              >
-                                                <span className={b('bullet')} />
-                                                <a
-                                                  href={issue.html_url}
-                                                  target="_blank"
-                                                >
-                                                  #{issue.ticket_number}
-                                                </a>{' '}
-                                                {issue.ticket_title},
-                                                <span className={b('lighter')}>
-                                                  closed:{' '}
-                                                  {format(
-                                                    new Date(issue.closed_at),
-                                                    'DD/MM/YYYY HH:mm'
-                                                  )}
-                                                  , last update:{' '}
-                                                  {format(
-                                                    new Date(issue.updated_at),
-                                                    'DD/MM/YYYY HH:mm'
-                                                  )}
-                                                </span>
-                                                {issue.assignees.map(user => (
-                                                  <img
-                                                    key={user.user_id}
-                                                    className={b('avatar')}
-                                                    src={user.avatar_url}
-                                                    alt="avatar"
-                                                    title={user.user_name}
-                                                  />
-                                                ))}
-                                              </li>
-                                            ))}
-                                        </ul>
-                                      </span>
-                                    )}
-                                  </span>
-                                )}
-                              <br />
-                              <input
-                                className="largeInput"
-                                id="milestones"
-                                name={milestone.milestone_title}
-                              />
-                            </li>
-                          ))}
-                      </ul>
+                      <ReportItems
+                        items={items}
+                        selectedCircle={selectedCircle}
+                      />
+                      <ReportProjects
+                        circleMilestones={circleMilestones}
+                        issues={issues}
+                        onMilestoneClick={onMilestoneClick}
+                      />
                     </span>
                   )}
                   <h3>Report content:</h3>
